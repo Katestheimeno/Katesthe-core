b24adc22aad33b3bb20ec9c165b727ea3f5c2f8c
Yeeloman <yami.onlyme@gmail.com>
2025-08-28 13:27:13 +0100

    update some admin tables to use unfold

--- accounts/admin/__init__.py
+++ accounts/admin/__init__.py
@@ -4,3 +4,35 @@ Path: accounts/admin/__init__.py
 """
 
 from ._user import *
+
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+from django.contrib.auth.models import Group
+from django_celery_beat.models import (
+    PeriodicTask, IntervalSchedule, CrontabSchedule, SolarSchedule, ClockedSchedule
+)
+
+# --- AUTH ---
+admin.site.unregister(Group)
+
+
+@admin.register(Group)
+class GroupAdmin(ModelAdmin):
+    pass
+
+
+# --- CELERY BEAT ---
+models_to_unfold = [
+    PeriodicTask,
+    IntervalSchedule,
+    CrontabSchedule,
+    SolarSchedule,
+    ClockedSchedule,
+]
+
+for model in models_to_unfold:
+    admin.site.unregister(model)
+
+    @admin.register(model)
+    class GenericUnfoldAdmin(ModelAdmin):
+        pass
